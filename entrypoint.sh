0490112283f20b5ba86ccfab6d27d70690302222
Muneeb Ahmad <muneeb131@gmail.com>
2021-10-29 03:55:05 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,24 +4,10 @@ suppressions=$1
 checklists=$2
 filters=$3
 continue_on_failure=$4
-use_custom_config=$5
-chkk_config_path=$6
-chkk_config_file=$7
 
-
-echo "$suppressions"
-echo "$checklists"
-echo "$continue_on_failure"
-echo "$use_custom_config"
-echo "$chkk_config_path"
-echo "$chkk_config_file"
-
-echo "printing current dir"
-pwd
 
 VERSION=$(curl -sS https://get.chkk.dev/helm/latest.txt) && curl -Lo chkk-post-renderer https://get.chkk.dev/${VERSION}/chkk-post-renderer-alpine
 chmod +x chkk-post-renderer
-
 
 if [ ${use_custom_config} == false ]; then
   touch config.yaml
@@ -35,7 +21,7 @@ if [ ${use_custom_config} == false ]; then
 EOF
 fi
 
-if [ ${checklists} != "[]" ] && [ ${use_custom_config} == false ]; then
+if [ ${checklists} != "[]" ]; then
   IFS=","; read -a checklistsArray <<< "$checklists";
   for index in "${!checklistsArray[@]}"; do
     val=${checklistsArray[index]};
@@ -43,7 +29,7 @@ if [ ${checklists} != "[]" ] && [ ${use_custom_config} == false ]; then
   done;
 fi
 
-if [ ${suppressions} != "[]" ] && [ ${use_custom_config} == false ]; then
+if [ ${suppressions} != "[]" ]; then
   IFS=","; read -a suppressionsArray <<< ${suppressions};
   for index in "${!suppressionsArray[@]}"; do
     val=${suppressionsArray[index]};
@@ -52,7 +38,7 @@ if [ ${suppressions} != "[]" ] && [ ${use_custom_config} == false ]; then
 fi
 
 
-if [ ${filters} != "[]" ] && [ ${use_custom_config} == false ]; then
+if [ ${filters} != "[]" ]; then
     IFS=","; read -a filtersArray <<< ${filters};
     for index in "${!filtersArray[@]}"; do
       val=${filtersArray[index]};
@@ -63,8 +49,6 @@ fi
 echo "::set-output name=config::config.yaml"
 echo "::set-output name=binary::chkk-post-renderer"
 
-# echo "CHKK_CONFIG_PATH=${chkk_config_path}" >> $GITHUB_ENV
-echo "CHKK_CONFIG_FILE=${chkk_config_file}" >> $GITHUB_ENV
 echo "CHKK_ACCESS_TOKEN=${CHKK_ACCESS_TOKEN}" >> $GITHUB_ENV
-
 echo "CHKK_CONFIG_PATH=/github/workspace" >> $GITHUB_ENV
+echo "CHKK_CONFIG_FILE=config.yaml" >> $GITHUB_ENV
