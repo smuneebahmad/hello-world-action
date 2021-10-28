d263c2cf72447da5983ea333e9a270c70f8de865
Muneeb Ahmad <muneeb131@gmail.com>
2021-10-29 00:32:54 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,9 +17,9 @@ echo "$chkk_config_path"
 echo "$chkk_config_file"
 
 
-VERSION=$(curl -sS https://get.chkk.dev/helm/latest.txt) && curl -Lo chkk-post-renderer https://get.chkk.dev/${VERSION}/chkk-post-renderer-alpine
+# VERSION=$(curl -sS https://get.chkk.dev/helm/latest.txt) && curl -Lo chkk-post-renderer https://get.chkk.dev/${VERSION}/chkk-post-renderer-alpine
 
-chmod +x chkk-post-renderer
+# chmod +x chkk-post-renderer
 
 if [ ${{ use-custom-config }} == false ]; then
   mkdir -p ${{ chkk-config-path }};
@@ -34,29 +34,29 @@ if [ ${{ use-custom-config }} == false ]; then
 EOF
 fi
 
-if [ ${{ checklists }} != "[]" ] && [ ${{ use-custom-config }} == false ]; then
-    IFS=","; read -a checklistsArray <<< ${{ checklists }};
-    for index in "${!checklistsArray[@]}"; do
-        val=${checklistsArray[index]};
-        yq eval ".checklists += "\"${val}\""" -i ${{ chkk-config-path }}/${{ chkk-config-file }};
-    done;
-fi
+# if [ ${{ checklists }} != "[]" ] && [ ${{ use-custom-config }} == false ]; then
+#     IFS=","; read -a checklistsArray <<< ${{ checklists }};
+#     for index in "${!checklistsArray[@]}"; do
+#         val=${checklistsArray[index]};
+#         yq eval ".checklists += "\"${val}\""" -i ${{ chkk-config-path }}/${{ chkk-config-file }};
+#     done;
+# fi
 
-if [ ${{ suppressions }} != "[]" ] && [ ${{ use-custom-config }} == false ]; then
-IFS=","; read -a suppressionsArray <<< ${{ suppressions }};
-    for index in "${!suppressionsArray[@]}"; do
-        val=${suppressionsArray[index]};
-        yq eval ".suppressions += "\"${val}\""" -i ${{ chkk-config-path }}/${{ chkk-config-file }};
-    done;
-fi
+# if [ ${{ suppressions }} != "[]" ] && [ ${{ use-custom-config }} == false ]; then
+# IFS=","; read -a suppressionsArray <<< ${{ suppressions }};
+#     for index in "${!suppressionsArray[@]}"; do
+#         val=${suppressionsArray[index]};
+#         yq eval ".suppressions += "\"${val}\""" -i ${{ chkk-config-path }}/${{ chkk-config-file }};
+#     done;
+# fi
 
-if [ ${{ filters }} != "[]" ] && [ ${{ use-custom-config }} == false ]; then
-    IFS=","; read -a filtersArray <<< ${{ filters }};
-    for index in "${!filtersArray[@]}"; do
-        val=${filtersArray[index]};
-        yq eval ".filters += "\"${val}\""" -i ${{ chkk-config-path }}/${{ chkk-config-file }};
-    done;
-fi
+# if [ ${{ filters }} != "[]" ] && [ ${{ use-custom-config }} == false ]; then
+#     IFS=","; read -a filtersArray <<< ${{ filters }};
+#     for index in "${!filtersArray[@]}"; do
+#         val=${filtersArray[index]};
+#         yq eval ".filters += "\"${val}\""" -i ${{ chkk-config-path }}/${{ chkk-config-file }};
+#     done;
+# fi
 
 
 
